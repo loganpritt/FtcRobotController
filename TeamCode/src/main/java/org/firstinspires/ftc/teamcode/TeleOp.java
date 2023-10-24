c11aee11616c1e0a19716e831ddf582d6b5d8ba3
loganpritt <lipritt8@gmail.com>
2023-10-24 16:16:05 -0700

    brogan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,4 +1,60 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.*;
+
+import org.firstinspires.ftc.teamcode.hardwareMaps.MecanumDrive;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "brogan")
+public class TeleOp extends OpMode {
+
+    //Declare the objects (all your motors/servos, etc.)
+
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
+    double xMovement;
+    double yMovement;
+    double rotation;
+    double drivePower;
+
+    org.firstinspires.ftc.teamcode.hardwareMaps.MecanumDrive drive;
+
+    public void init() {
+
+        //Initialize the objects
+
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        drive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
+
+    }
+
+    public void loop() {
+
+        xMovement = gamepad1.left_stick_x;
+        yMovement = gamepad1.left_stick_y;
+        rotation = gamepad1.right_stick_x;
+        drivePower = 0.8 * Math.max(Math.max(Math.abs(xMovement), Math.abs(yMovement)), Math.abs(rotation));
+
+        drive.moveInTeleop(xMovement, yMovement, rotation, drivePower);
+
+        }
+
+    }
+
 }
